f36aac3e85e667402d68a30f6700ba12029f610d
Thomas Zub <thomas.zub@codecentric.de>
2021-12-08 13:34:03 +0100

    Fehlermeldung, falls Verbindung zum Server nicht hergestellt werden kann

--- java-libuv-client/src/main/java/Client.java
+++ java-libuv-client/src/main/java/Client.java
@@ -12,6 +12,9 @@ public class Client implements Closeable {
 
     public Client(InetAddress address, int port) throws IOException {
         this.socket = new Socket(address, port);
+        if (!socket.isConnected()) {
+            throw new IOException(String.format("Konnte keine Verbindung zum Server %s:%d herstellen", address, port));
+        }
         this.writer = new PrintWriter(socket.getOutputStream(), true);
         this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
