57742a7b9add8872b339443c01bad3fab0f78a0f
Thomas Zub <thomas.zub@codecentric.de>
2021-12-08 14:25:23 +0100

    Moved to package

--- java-libuv-client/src/main/java/one/zub/java_libuv_client/App.java
+++ java-libuv-client/src/main/java/one/zub/java_libuv_client/App.java
@@ -1,3 +1,5 @@
+package one.zub.java_libuv_client;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.time.Duration;
--- java-libuv-client/src/main/java/one/zub/java_libuv_client/Client.java
+++ java-libuv-client/src/main/java/one/zub/java_libuv_client/Client.java
@@ -1,3 +1,5 @@
+package one.zub.java_libuv_client;
+
 import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
